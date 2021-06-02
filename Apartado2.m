1e29c3b7866a5d16c7be4484b712418ef97f7163
Char Llorente Cortijo <cj.llorente@alumnos.upm.es>
2021-06-02 12:56:44 +0200

    +*

--- Apartado2.m
+++ Apartado2.m
@@ -1,149 +1,185 @@
 close all; clear all;
 
-%% Inicializar valore
+%% Inicializar valores
+reductora = 23;
 p = 64.986;
-K = 2652.28;
+K = 2652.28*reductora;
 
-experimentosbeta2 = 20;
-beta2min = 10;
-beta2max = 15;
+minResol = 0.005;
 
-experimentosbeta = 20;
-betamin = 0;
-betamax = 5;
+experimentosKp = 50;
+Kpmin = -6;
+% Kpmin = minResol;
+Kpmax = 6;
+% Kpmax = 100;
 
-experimentoszeta = 20;
-zetamin = 2;
-zetamax = 10;
+experimentosTd1 = 50;
+Td1min = -10;
+% Td1min = -2;
+Td1max = 6;
 
-experimentosomega = 1;
-omegamin = 25;
-omegamax = 25;
+experimentosTi = 50;
+Timin = -10;
+% Timin = 0;
+Timax = 10;
 
-[betas2, betas, zetas, omegas] = ndgrid(   linspace(beta2min, beta2max, experimentosbeta2),...
-                                        linspace(betamin, betamax, experimentosbeta),...
-                                        linspace(zetamin, zetamax, experimentoszeta),...
-                                        linspace(omegamin, omegamax, experimentosomega));
-                                    
+% [kps, Td1s, Tis] = ndgrid(  linspace(Kpmin, Kpmax, experimentosKp),...
+%                             linspace(Td1min, Td1max, experimentosTd1),...
+%                             linspace(Timin, Timax, experimentosTi));
+                        
+[kps, Td1s, Tis] = ndgrid(  exp(linspace(Kpmin, Kpmax, experimentosKp)),...
+                            exp(linspace(Td1min, Td1max, experimentosTd1)),...
+                            exp(linspace(Timin, Timax, experimentosTi)));
+                        
 n = 1;
     
 % Init somethings
 
-x = p.*(0:0.0005/p:0.05);
+x = p.*(0:0.0001/p:0.05);
 u = ones(1,length(x));
 u(1:ceil(length(u)/6)) = 0;
 t = x;
 
 Mpmin = 0.08;
+% Mpmin = 0;
+% Mpmax = 0.15;
 Mpmax = 0.15;
 
 tsmax = 0.5 + t(ceil(length(u)/6)); %segundos
 tolerancia = 0.02;
 
 trmax = 0.3 + t(ceil(length(u)/6)); %segundos
-trmin = 0.09 + t(ceil(length(u)/6)); %segundos
+trmin = -0.1 + t(ceil(length(u)/6)); %segundos
             
-for b2 = 1: experimentosbeta2
-    for b = 1: experimentosbeta
-        for z = 1: experimentoszeta
-            for o = 1: experimentosomega
+for kpi = 1: experimentosKp
+    for td1i = 1: experimentosTd1
+        for tii = 1: experimentosTi
     
-%                 disp(['beta2 ' num2str(b2) ' beta ' num2str(b) ' zeta ' num2str(z) ' omega ' num2str(o)]);
+%           disp(['Kp ' num2str(Kp) ' Td1 ' num2str(Td1) ' Td2 ' num2str(Td2) ' Ti ' num2str(Ti)]);
 
-                beta2 = betas2(b2,b,z,o);
-                beta = betas(b2,b,z,o);
-                zeta = zetas(b2,b,z,o);
-                omega = omegas(b2,b,z,o);
-                
-                disp(['n ' num2str(n) ' de ' num2str(experimentosomega*experimentoszeta*experimentosbeta*experimentosbeta2)]);
-%                 disp(['beta2 ' num2str(beta2) ' beta ' num2str(beta) ' zeta ' num2str(zeta) ' omega ' num2str(omega)]);
-                n = n + 1;
-            %% Calculo de parametros
+            Kp = kps(kpi,td1i,tii);
+            Td1 = Td1s(kpi,td1i,tii);
+            Ti = Tis(kpi,td1i,tii);
 
-                Kp = (p^2*(2*beta+1/zeta^2)) / (beta2^2*K);
-                Td1 = (beta2*(beta+2)) / (p*(2*beta+1/zeta^2));
-                Td2 = -p/(K*Kp);
-                Ti = (beta2*zeta^2*(2*beta+1/zeta^2)) / (beta*p);
-                Td = Td1+Td2;
+            disp([num2str(n) '/' num2str(experimentosKp*experimentosTd1*experimentosTi)]);
+%           %disp(['Kp ' num2str(Kp) ' Td1 ' num2str(Td1) ' Td2 ' num2str(Td2) ' Ti ' num2str(Ti)]);
+            n = n + 1;
+        %% Calculo de parametros
 
-                num = [K*Kp*Td1 K*Kp K*Kp/Ti];
-                den = [1 p+K*Kp*Td K*Kp K*Kp/Ti];
-                sys = tf(num,den);
+            Td2 = -p/(K*Kp);
+            Td = Td1+Td2;
 
-                %% Condiciones de estabilidad
-                if Kp < 0
-                    disp(['Kp < 0 ' 'beta2 ' num2str(b2) ' beta ' num2str(b) ' zeta ' num2str(z) ' omega ' num2str(o)]);
-                    continue
-                    % no estable
-                end
+            num = [K*Kp*Td1 K*Kp K*Kp/Ti];
+            den = [1 p+K*Kp*Td K*Kp K*Kp/Ti];
+            sys = tf(num,den);
 
-                if Ti < 0
-                    disp(['Ti < 0 ' 'beta2 ' num2str(b2) ' beta ' num2str(b) ' zeta ' num2str(z) ' omega ' num2str(o)]);
-                    continue
-                    % no estable
-                end
+            %% Condiciones de estabilidad
+            epsilon = p+K*Kp*Td;
 
-                if Kp*Td <= -p/K
-                    disp(['Kp*Td <= -p/K ' 'beta2 ' num2str(b2) ' beta ' num2str(b) ' zeta ' num2str(z) ' omega ' num2str(o)]);
-                    continue
-                    % no estable
-                end
-
-                epsilon = p+K*Kp*Td;
-
-                if epsilon <= 0
-                    disp(['epsilon <= 0 ' 'beta2 ' num2str(b2) ' beta ' num2str(b) ' zeta ' num2str(z) ' omega ' num2str(o)]);
-                    continue
-                    % no estable
-                end
-
-                %% Respuesta ante el escalon
-
-                y = lsim(sys, u, t);
-                
-                %% Condicion de sobreelongacion maxima y minima
-                
-                if sum(y > (1 + Mpmax)) >= 1
-                    disp(['Cond. sobreelongacion max ' 'beta2 ' num2str(b2) ' beta ' num2str(b) ' zeta ' num2str(z) ' omega ' num2str(o)]);
-                    continue
-                end
-                
-                if sum(y > (1 + Mpmin)) == 0
-                    disp(['Cond. sobreelongacion min ' 'beta2 ' num2str(b2) ' beta ' num2str(b) ' zeta ' num2str(z) ' omega ' num2str(o)]);
-                    continue
-                end
-                    
-                %% Condicion de tiempo de establecimiento
-                                
-                if sum(y(find(t >= tsmax,1):end) > (1 + tolerancia)) >= 1
-                    disp(['Cond. Ts max ' 'beta2 ' num2str(b2) ' beta ' num2str(b) ' zeta ' num2str(z) ' omega ' num2str(o)]);
-                    continue
-                end
-                
-                if sum(y(find(t >= tsmax,1):end) < (1 - tolerancia)) >= 1
-                    disp(['Cond. Ts min ' 'beta2 ' num2str(b2) ' beta ' num2str(b) ' zeta ' num2str(z) ' omega ' num2str(o)]);
-                    continue
-                end
-
-                %% Condicion de tiempo de subida
-                                
-                if sum(t(find(y >= 1,1)) >= trmax) >= 1
-                    disp(['Cond. tiempo subida max ' 'beta2 ' num2str(b2) ' beta ' num2str(b) ' zeta ' num2str(z) ' omega ' num2str(o)]);
-                    continue
-                end
-                                               
-                if sum(t(find(y >= 1,1)) <= trmin) >= 1
-                    disp(['Cond. tiempo subida min ' 'beta2 ' num2str(b2) ' beta ' num2str(b) ' zeta ' num2str(z) ' omega ' num2str(o)]);
-                    continue
-                end
-                
-                % Plot
-                info = ['beta2 ' num2str(beta2) ' beta ' num2str(beta) ' zeta ' num2str(zeta) ' omega ' num2str(omega)];
-                h = plot(t,y, 'DisplayName', info);
-                h.DataTipTemplate.DataTipRows(end+1) = dataTipTextRow('Trace',repmat({h.DisplayName},size(h.XData)));
-
-                hold on;
+            if Kp < 0
+%                 disp(['Kp < 0 ' 'Kp ' num2str(Kp) ' Td1 ' num2str(Td1) ' Td2 ' num2str(Td2) ' Ti ' num2str(Ti)]);
+                continue
+                % no estable
             end
+
+            if Ti < 0
+%                 disp(['Ti < 0 ' 'Kp ' num2str(Kp) ' Td1 ' num2str(Td1) ' Td2 ' num2str(Td2) ' Ti ' num2str(Ti)]);
+                continue
+                % no estable
+            end
+
+            if Kp*K*(1-1/(Ti*epsilon)) < 0
+%                 disp(['Kp*K*(1-1/(Ti*epsilon)) < 0 ' 'Kp ' num2str(Kp) ' Td1 ' num2str(Td1) ' Td2 ' num2str(Td2) ' Ti ' num2str(Ti)]);
+                continue
+                % no estable
+            end
+
+            if epsilon <= 0
+%                 disp(['epsilon <= 0 ' 'Kp ' num2str(Kp) ' Td1 ' num2str(Td1) ' Td2 ' num2str(Td2) ' Ti ' num2str(Ti) ' epsilon ' num2str(epsilon)]);
+                continue
+                % no estable
+            end
+
+            %% Respuesta ante el escalon
+
+            y = lsim(sys, u, t);
+
+            %% Condicion de sobreelongacion maxima y minima
+
+            if sum(y > (1 + Mpmax)) >= 1
+%                 disp(['Cond. sobreelongacion max ' 'Kp ' num2str(Kp) ' Td1 ' num2str(Td1) ' Td2 ' num2str(Td2) ' Ti ' num2str(Ti)]);
+                continue
+            end
+
+            if sum(y > (1 + Mpmin)) == 0
+%                 disp(['Cond. sobreelongacion min ' 'Kp ' num2str(Kp) ' Td1 ' num2str(Td1) ' Td2 ' num2str(Td2) ' Ti ' num2str(Ti)]);
+                continue
+            end
+
+            % Condicion de tiempo de establecimiento
+
+            if sum(y(find(t >= tsmax,1):end) > (1 + tolerancia)) >= 1
+%                 disp(['Cond. Ts max ' 'Kp ' num2str(Kp) ' Td1 ' num2str(Td1) ' Td2 ' num2str(Td2) ' Ti ' num2str(Ti)]);
+                continue
+            end
+
+            if sum(y(find(t >= tsmax,1):end) < (1 - tolerancia)) >= 1
+%                 disp(['Cond. Ts min ' 'Kp ' num2str(Kp) ' Td1 ' num2str(Td1) ' Td2 ' num2str(Td2) ' Ti ' num2str(Ti)]);
+                continue
+            end
+
+            %% Condicion de tiempo de subida
+
+            if sum(t(find(y >= 1,1)) >= trmax) >= 1
+%                 disp(['Cond. tiempo subida max ' 'Kp ' num2str(Kp) ' Td1 ' num2str(Td1) ' Td2 ' num2str(Td2) ' Ti ' num2str(Ti)]);
+                continue
+            end
+
+            if sum(t(find(y >= 1,1)) <= trmin) >= 1
+%                 disp(['Cond. tiempo subida min ' 'Kp ' num2str(Kp) ' Td1 ' num2str(Td1) ' Td2 ' num2str(Td2) ' Ti ' num2str(Ti)]);
+                continue
+            end
+            
+            %% Condiciones resolucion
+            
+            if abs(Kp) < minResol
+                continue
+            end
+            
+            if abs(Td1*Kp) < minResol
+                continue
+            end
+            
+%             if abs(Td2*Kp) < minResol
+%                 continue
+%             end
+            
+            if abs(Kp/Ti) < minResol
+                continue
+            end
+
+            if abs(Kp) > 999
+                continue
+            end
+            
+            if abs(Td1*Kp) > 999
+                continue
+            end
+            
+            if abs(Td2*Kp) > 999
+                continue
+            end
+            
+            if abs(Kp/Ti) > 999
+                continue
+            end
+
+            %% Plot
+            info = ['Kp ' num2str(Kp) ' Kd1 ' num2str(Kp*Td1) ' Kd2 ' num2str(Kp*Td2) ' Ki ' num2str(Kp/Ti)];
+            h = plot(t,y, 'DisplayName', info);
+            h.DataTipTemplate.DataTipRows(end+1) = dataTipTextRow('Trace',repmat({h.DisplayName},size(h.XData)));
+
+            hold on;
         end
     end
 end
